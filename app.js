70fc80975538bd2df40e84da8bfb6b64134803a6
CarnageXD <40719523+CarnageXD@users.noreply.github.com>
2020-11-29 22:43:05 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 "use strict"
-const key = "48cb0ce7073e9c7319bf9870cc0582e4";
+const key = "74d929a73e3642fda3a2696e947bfd22";
 const search = document.querySelector(".form-menu");
 const searchTheme = document.querySelector(".form-theme")
-const details = document.querySelector(".details");
+const mainLoad = document.querySelector("main");
 const prevButton = document.querySelector(".button-left")
 const nextButton = document.querySelector(".button-right")
 const mainTheme = document.querySelector('.main-theme')
@@ -14,10 +14,12 @@ let yyyy = today.getFullYear();
 today = dd + "/" + mm + "/" + yyyy;
 
 
-
+// alert(`Для тестировщиков
+// В планах: эффекты мб (в зависимости от погоды), смена оформления (или тоже от погоды или от дня) звук (от погоды и время дня), переоформление подачи самой погоды 100%, ща так себе..И адаптивность под разные разрешения.
+// Мб чет еще?`)
 search.addEventListener("submit", (event) => {
     event.preventDefault();
-    details.innerHTML = `<img height="150" width="150" src="weather_icons/refresh.gif">`;
+    // details.innerHTML = `<img height="150" width="150" src="weather_icons/refresh.gif">`;
     const location = event.target.location.value;
     weatherApp(location);
 });
@@ -25,7 +27,7 @@ search.addEventListener("submit", (event) => {
 searchTheme.addEventListener("submit", (event) => {
     event.preventDefault();
     mainTheme.classList.add('hide')
-    details.innerHTML = `<img height="150" width="150" src="weather_icons/refresh.gif">`;
+    // details.innerHTML = `<img height="150" width="150" src="weather_icons/refresh.gif">`;
     const location = event.target.location.value;
     weatherApp(location);
 });
@@ -36,33 +38,137 @@ async function weatherApp(location) {
 }
 
 async function fetchAPI(location) {
-    const baseURL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&metric&lang=ua`;
+    const baseURL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${location}&key=${key}&days=${7}&lang=uk`
     const response = await fetch(baseURL);
     const data = await response.json();
     console.log(data);
     return data;
 }
 
+
 function generateHTML(data) {
     if (data.cod != '404') {
-        prevButton.classList.add('show')
-        nextButton.classList.add('show')
         const html = `
-    <div class="first-sight-info">
-                    <div class="location-data">${data.name}</div>
-                    <div class="date-data">${today}</div>
-                    <div class="temperature">${Math.round(data.main.temp - 273)}<span>°с</span></div>
-                    <div class="description">${data.weather[0].description}</div>
-                    <div class="feels-like">Почувається як: ${Math.round(data.main.feels_like - 273)}<span>°с</span></div                                       
-                    <div class="img-description"><img src = https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png></div>
+        <div class="container">
+            <div class="details">
+                <div class="first-sight-info">
+                    <div class="degree-description">
+                        <div class="degree">
+                            <div class="temperature">0<span>°с</span></div>
+                            <div class="description">хмарно</div>
+                        </div>
+                        <div class="additional-degree">
+                            <div class="humidity">
+                                <div class="additional-degree-name">Вологіcть</div>
+                                <div class="additional-degree-index">90%</div>
+                            </div>
+                            <div class="separator"> | </div>
+                            <div class="wind">
+                                <div class="additional-degree-name">Вітер</div>
+                                <div class="additional-degree-index">1 м/с</div>
+                            </div>
+
+                        </div>
+                        <div class="img-description"></div>
+                    </div>
+                    <div class="geolocation">
+                        <div class="location-data">${data.city_name}</div>
+                        <div class="date-data">${data.data[0].valid_date}</div>
+                    </div>
                 </div>
-                <div class="secondary-info">
-                    <p>Вологість - ${data.main.humidity}%</p>
-                    <p>Швидкість вітру - ${data.wind.speed} м/с</p>                                       
+            </div>
+
+        </div>
+        <div class="additional-container">
+            <section class="weather">
+                <div class="daycontainer">
+                    <div class="week-day">${cutDate(data.data[0].valid_date)}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[0].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[0].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[0].weather.description}</div>
                 </div>
-    `;
-        details.innerHTML = html;
+                <div class="daycontainer">
+                    <div class="week-day">${cutDate(data.data[1].valid_date)}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[1].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[1].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[1].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${cutDate(data.data[2].valid_date)}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[2].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[2].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[2].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${cutDate(data.data[3].valid_date)}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[3].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[3].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[3].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${cutDate(data.data[4].valid_date)}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[4].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[4].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[4].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${cutDate(data.data[5].valid_date)}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[5].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[5].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[5].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                <div class="week-day">${cutDate(data.data[6].valid_date)}</div>
+                <div class="img-description">
+                    <img src="weather_icons/${data.data[6].weather.icon}.png">
+                </div>
+                <div class="week-day-temperature">${data.data[6].temp}<span>°с</span></div>
+                <div class="week-day-description">${data.data[6].weather.description}</div>
+            </div>
+            </section>
+        </div>
+`;
+        mainLoad.innerHTML = html;
     }
     else alert('Дане місцеположення не знайдене, перевірте його написання')
 }
+
+function cutDate(data) {
+    return data.split('-').splice(1, 2).reverse().join('.')
+}
+
+// function generateHTML(data) {
+//     if (data.cod != '404') {
+//         // prevButton.classList.add('show')
+//         // nextButton.classList.add('show')
+//         const html = `
+//         <div class="first-sight-info">
+//                         <div class="location-data">${data.city_name}</div>
+//                         <div class="date-data">${today}</div>
+//                         <div class="temperature">${data.data[0].temp}<span>°с</span></div >
+//                         <div class="description">${data.data[0].weather.description}</div>
+//                         <div class="feels-like">Почувається як ${data.data[0].app_temp}</span></div                                       
+//                         <div class="img-description"></div>
+//                     </div >
+//     <div class="secondary-info">
+//         <p>Вологість - ${data.data[0].rh}% </p>
+//         <p>Хмарність: ${data.data[0].clouds}%</p>
+//     </div>
+// `;
+//         details.innerHTML = html;
+//     }
+//     else alert('Дане місцеположення не знайдене, перевірте його написання')
+// }
 
--- geoApp.js
+++ geoApp.js
@@ -15,7 +15,7 @@ geolocationButton.addEventListener('click', (event) => {
     }
 
     async function fetchAPI(lat, lon) {
-        const geoURL = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+        const geoURL = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&key=${key}&days=${7}&lang=uk`
         const response = await fetch(geoURL);
         const data = await response.json();
         console.log(data);
@@ -24,26 +24,99 @@ geolocationButton.addEventListener('click', (event) => {
 
     function generateHTML(data) {
         const html = `
-        <div class="first-sight-info">
-                        <div class="location-data">${data.name}</div>
-                        <div class="date-data">${today}</div>
-                        <div class="temperature">${Math.round(data.main.temp - 273)}<span>°с</span></div>
-                        <div class="description">${data.weather[0].description}</div>
-                        <div class="feels-like">Почувається як: ${Math.round(data.main.feels_like - 273)}<span>°с</span></div                                       
-                        <div class="img-description"><img src = https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png></div>
+        <div class="container">
+            <div class="details">
+                <div class="first-sight-info">
+                    <div class="degree-description">
+                        <div class="degree">
+                            <div class="temperature">0<span>°с</span></div>
+                            <div class="description">хмарно</div>
+                        </div>
+                        <div class="additional-degree">
+                            <div class="humidity">
+                                <div class="additional-degree-name">Вологіcть</div>
+                                <div class="additional-degree-index">90%</div>
+                            </div>
+                            <div class="separator"> | </div>
+                            <div class="wind">
+                                <div class="additional-degree-name">Вітер</div>
+                                <div class="additional-degree-index">1 м/с</div>
+                            </div>
+
+                        </div>
+                        <div class="img-description"></div>
                     </div>
-                    <div class="secondary-info">
-                        <p>Вологість - ${data.main.humidity}%</p>
-                        <p>Швидкість вітру - ${data.wind.speed} м/с</p>                                       
+                    <div class="geolocation">
+                        <div class="location-data">${data.city_name}</div>
+                        <div class="date-data">${data.data[0].valid_date}</div>
                     </div>
-        `;
-        details.innerHTML = html;
+                </div>
+            </div>
+
+        </div>
+        <div class="additional-container">
+            <section class="weather">
+                <div class="daycontainer">
+                    <div class="week-day">${data.data[0].valid_date}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[0].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[0].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[0].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${data.data[1].valid_date}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[1].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[1].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[1].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${data.data[2].valid_date}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[2].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[2].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[2].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${data.data[3].valid_date}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[3].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[3].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[3].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${data.data[4].valid_date}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[4].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[4].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[4].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                    <div class="week-day">${data.data[5].valid_date}</div>
+                    <div class="img-description">
+                        <img src="weather_icons/${data.data[5].weather.icon}.png">
+                    </div>
+                    <div class="week-day-temperature">${data.data[5].temp}<span>°с</span></div>
+                    <div class="week-day-description">${data.data[5].weather.description}</div>
+                </div>
+                <div class="daycontainer">
+                <div class="week-day">${data.data[6].valid_date}</div>
+                <div class="img-description">
+                    <img src="weather_icons/${data.data[6].weather.icon}.png">
+                </div>
+                <div class="week-day-temperature">${data.data[6].temp}<span>°с</span></div>
+                <div class="week-day-description">${data.data[6].weather.description}</div>
+            </div>
+            </section>
+        </div>
+    `;
+        mainLoad.innerHTML = html;
         mainTheme.classList.add('hide')
     }
-    if (Object.cod != '404') {
-        prevButton.classList.add('show')
-        nextButton.classList.add('show')
-    }
-
 })
 
